7eb51885074a6127e5cfa401ae18e874123d5910
Haydar Rezaie <haydar.rezaie400@gmail.com>
2025-11-28 18:49:07 -0500

    trigger CI 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World from Dockerized Python app!"
+    return "This App is deployed by pipeline on Docker!"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Use Render's PORT if available
